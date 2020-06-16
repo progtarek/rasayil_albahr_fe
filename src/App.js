9ef1b065e6df88dfcd0ebb78246236526c0c4921
progtarek <progtarek@gmail.com>
2020-06-17 01:00:08 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import LandingPage from './modules/landing/landing-page.component';
+import LoginPage from './modules/auth/login-page/login-page.component';
+import RegisterPage from './modules/auth/register-page/register-page.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/login' component={LoginPage}></Route>
+        <Route exact path='/register' component={RegisterPage}></Route>
+        <Route exact path='/' component={LandingPage}></Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/modules/landing/landing-page.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>landing page 2</div>;
+};
+
+export default LandingPage;
