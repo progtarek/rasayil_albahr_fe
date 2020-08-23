ef6ccd4382075b12ba52f772b7c4ebfb042d93e5
tarek <progtarek@gmail.com>
2020-08-24 01:15:32 +0200

    fix redirecting with social

--- src/modules/auth/social-login-page/social-login-page.jsx
+++ src/modules/auth/social-login-page/social-login-page.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { PageContainer } from '../../../shared/styles/global.styles';
 import { useLocation } from 'react-router-dom';
-import { push } from 'connected-react-router';
 
-const SocialLoginPage = () => {
+const SocialLoginPage = ({ history }) => {
   const searchParams = new URLSearchParams(useLocation().search);
+
   const token = searchParams.get('token');
   const username = searchParams.get('username');
   const profilePictureUrl = searchParams.get('profilePictureUrl');
-  console.log('========================');
-  console.log(token, username, profilePictureUrl);
-  console.log('========================');
-  searchParams.delete(token);
-  searchParams.delete(username);
-  searchParams.delete(profilePictureUrl);
-  window.localStorage.setItem('token', token);
-  window.localStorage.setItem('username', username);
-  window.localStorage.setItem('profilePictureUrl', username);
+
+  window.localStorage.setItem('token', decodeURIComponent(token));
+  window.localStorage.setItem('username', decodeURIComponent(username));
+  window.localStorage.setItem(
+    'profilePictureUrl',
+    decodeURIComponent(profilePictureUrl)
+  );
+
   if (!token || !username) {
-    push('/');
+    history.push('/login');
   } else {
-    push('/account');
-    push('/account');
+    history.push('/account');
   }
 
   return <PageContainer>...loading</PageContainer>;
